9300b48f86b6ff8c4a16cf825a5fddacc3142a25
lmartinezs <martinez.s.luis@gmail.com>
2021-10-31 18:47:47 +0100

    Labels in layout

--- src/components/ColorInput.vue
+++ src/components/ColorInput.vue
@@ -41,8 +41,6 @@ export default {
 input[type='color'] {
   -webkit-appearance: none;
   border: none;
-  width: 32px;
-  height: 32px;
 }
 input[type='color']::-webkit-color-swatch-wrapper {
   padding: 0;
--- src/components/ColorScheme.vue
+++ src/components/ColorScheme.vue
@@ -3,15 +3,22 @@
     v-for="(colorGroup, colorGroupIdx) in scheme"
     :key="colorGroup.label"
     :colorGroup="colorGroup"
-    class="flex space-x-1 mb-4"
   >
-    <ColorInput
-      class="w-8 h-8 rounded shadow cursor-pointer overflow-hidden"
-      v-for="(shade, shadeIdx) in colorGroup.shades"
-      :key="`${colorGroup.label}${shade.label}`"
-      :color="shade"
-      @update:color="newColor => onUpdateColor(newColor, colorGroupIdx, shadeIdx)"
-    />
+    <div class="mb-3 capitalize text-gray-600">{{colorGroup.label}}</div>
+    <div class="flex space-x-1 mb-4">
+      <div
+        v-for="(shade, shadeIdx) in colorGroup.shades"
+        :key="`${colorGroup.label}${shade.label}`"
+        class="flex flex-col items-center"
+      >
+        <ColorInput
+          class="w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 rounded shadow cursor-pointer overflow-hidden mb-1"
+          :color="shade"
+          @update:color="newColor => onUpdateColor(newColor, colorGroupIdx, shadeIdx)"
+        />
+        <div class="text-sm text-gray-500">{{shade.label}}</div>
+      </div>
+    </div>
   </ColorGroup>
 </template>
 
